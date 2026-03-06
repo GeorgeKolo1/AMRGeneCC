f9d12230e9eca69042c69137b89295233b2ec9ab
georgekolo1 <georgemkhd@gmail.com>
2026-03-06 07:46:59 +0000

    Changed folder layout, flat layout is probably better.

---
+++ amrgene/cli.py
@@ -0,0 +1,19 @@
+import argparse
+import sys
+
+from amrgene.amr_gene_script import AMR_gene_concat
+
+def main():
+    parser = argparse.ArugmentParser(
+        description='Concatenate AMR gene results from RGI output files'
+    )
+    parser.add_argument(
+        'folder',
+        help='Path to the folder containing your AMRFinder results file (should end in .fasta.gz.txt)',
+    )
+
+    args = parser.parse_args()
+    AMR_gene_concat(args.folder)
+
+if __name__ == "__main__":
+    main()
